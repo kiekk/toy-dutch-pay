0cc68484f3ce840c28eca854f8bc08fcd3353ce5
shyoon <shyoon991@gmail.com>
2023-07-10 12:52:46 +0900

    Initial Commit

---
+++ dutch-pay-api/src/test/java/com/pay/dutchpayapi/DutchPayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.dutchpayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutchPayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
